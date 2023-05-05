2d254e5f2cc090e2ccbc6f1f68473e319f70cbc5
kornicameister <kornicameister@gmail.com>
2023-05-05 14:28:18 +0200

    Configure brew for macos

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# homebrew
+if status --is-interactive
+  eval (/usr/local/bin/brew shellenv)
 end
+set -x HOMEBREW_NO_ANALYTICS 1
+set -x HOMEBREW_NO_ENV_HINTS 1
