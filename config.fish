35805fab30e6ee8a5236e6655455573bf1375856
kornicameister <kornicameister@gmail.com>
2023-05-05 14:28:34 +0200

    Use nvim as editor + add aliases

--- config.fish
+++ config.fish
@@ -4,3 +4,10 @@ if status --is-interactive
 end
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_ENV_HINTS 1
+
+# editor
+alias vi    'nvim'
+alias vim   'nvim'
+alias view  'nvim -R'
+set -x EDITOR nvim
+set -x VISUAL $EDITOR
